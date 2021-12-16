bfa0f817f46fd0406c8f170cde00988eb9277712
Jan Czok <jan.czok@outlook.de>
2021-12-16 10:14:47 +0100

    html and css

---
+++ index.js
@@ -0,0 +1,7 @@
+function showGift(){
+    // TODO: prüfen, ob der Tag in der Vergangenheit oder Gegenwart liegt, dann Zitat anzeigen, sost nicht
+
+    // TODO: Für jeden Tag ein Zitat
+
+    // TODO: Das Zitat anzeigen
+}
